a571cb7be70cf9321330630638f84db7f8223418
Paul Klingman <paul@reacro.com>
2017-10-30 11:23:06 -0400

    (feature) Create EventList class. Stage Simulators project.

---
+++ src/BlackjackSimulator.Abstractions/EventList.cs
@@ -0,0 +1,30 @@
+﻿namespace BlackjackSimulator.Abstractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class EventList<T> : Collection<T>
+    {
+        public event EventHandler ItemAdding;
+        public event EventHandler ItemAdded;
+
+        public new void Add(T item)
+        {
+            if (this.ItemAdding != null)
+            {
+                this.ItemAdding(item, EventArgs.Empty);
+            }
+
+            base.Add(item);
+
+            if (this.ItemAdded != null)
+            {
+                this.ItemAdded(item, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ src/BlackjackSimulator.Abstractions/Model/BlackjackHand.cs
@@ -0,0 +1,34 @@
+﻿namespace BlackjackSimulator.Abstractions.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BlackjackHand
+    {
+        /// <summary>
+        /// Represents the cards in the hand.
+        /// </summary>
+        //// TODO: Can this be unordered? E.g. HashSet?
+        public EventList<PlayingCard> Cards { get; private set; } = new EventList<PlayingCard>();
+
+        public HashSet<int> Values { get; private set; } = new HashSet<int>();
+
+        public BlackjackHand()
+        {
+            this.Cards.ItemAdded += Cards_ItemAdded;
+        }
+
+        private void Cards_ItemAdded(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SingleValue()
+        {
+            return (this.Values.Count == 1);
+        }
+    }
+}
